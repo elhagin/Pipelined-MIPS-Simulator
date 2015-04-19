063fb2e52debb0752e4bd66abb646ac39a7f4e32
elhagin <omar.elhagin@live.com>
2015-04-19 15:00:53 +0200

    added constructor

--- MIPS/src/mainUnits/ALU.java
+++ MIPS/src/mainUnits/ALU.java
@@ -2,7 +2,13 @@ package mainUnits;
 
 public class ALU
 {
-	
+	private int input1;
+	private int input2;
+	public ALU(Register previousReg, Register nextReg)
+	{
+		input1 = previousReg.getData1(); //to be adjusted after registers are implemented
+		input2 = previousReg.getData2();
+	}
 	public int generateControl(int aluOpControlSignal, int funct)
 	{
 		int aluControl = 8;
@@ -30,5 +36,17 @@ public class ALU
 		}
 		
 		return aluControl;
+	}
+	public int getInput1() {
+		return input1;
+	}
+	public void setInput1(int input1) {
+		this.input1 = input1;
+	}
+	public int getInput2() {
+		return input2;
+	}
+	public void setInput2(int input2) {
+		this.input2 = input2;
 	}
 }
